0a93653c799a0f281c4ee614f21efd994994377a
Melissa W <mwebdesign@icloud.com>
2015-12-04 14:15:53 -0500

    Got location

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView?
+    
+    var locationManager: CLLocationManager?
+    let distanceSpan: Double = 500
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let mapView = self.mapView {
+            mapView.delegate = self
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50
+            locationManager!.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            mapView.setRegion(region, animated: true)
+        }
     }
 
 
